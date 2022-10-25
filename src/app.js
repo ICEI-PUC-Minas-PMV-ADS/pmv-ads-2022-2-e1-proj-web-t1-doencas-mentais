5c3d7c38a516ef77ea76247573b88c2e23f813b5
Diego Gomes <diegomjua@gmail.com>
2022-10-24 22:23:47 -0300

    Navbar e esqueleto

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/**
+ * Inicio da animação para o hamburger menu mobile
+ */
+const menuMobile = document.querySelector('#menuMobile');
+const menuMobileLinks = document.querySelector('.menuItens');
+
+menuMobile.addEventListener('click', function() {
+    menuMobile.classList.toggle('is-active');
+    menuMobileLinks.classList.toggle('active');
+})
+/**
+ * Fim da animação
+ */
